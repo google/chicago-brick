3d367a7706d99edc2cb8c2d55fcf27ad89056087
Brian Turnbull <brian.m.turnbull@gmail.com>
2017-09-17 10:02:49 -0500

    Replace tabs from borrowed code with spaces. Fix some long lines that I added.

--- demo_modules/wind/color.js
+++ demo_modules/wind/color.js
@@ -22,11 +22,11 @@ const color = require('demo_modules/wind/color');
 const τ = 2 * Math.PI;
 
 function colorInterpolator(start, end) {
-	var r = start[0], g = start[1], b = start[2];
-	var Δr = end[0] - r, Δg = end[1] - g, Δb = end[2] - b;
-	return function(i, a) {
-		return [Math.floor(r + i * Δr), Math.floor(g + i * Δg), Math.floor(b + i * Δb), a];
-	};
+  var r = start[0], g = start[1], b = start[2];
+  var Δr = end[0] - r, Δg = end[1] - g, Δb = end[2] - b;
+  return function(i, a) {
+    return [Math.floor(r + i * Δr), Math.floor(g + i * Δg), Math.floor(b + i * Δb), a];
+  };
 }
 
 /**
@@ -38,17 +38,17 @@ function colorInterpolator(start, end) {
  * @returns {Array} [r, g, b, a]
  */
 function sinebowColor(hue, a) {
-	// Map hue [0, 1] to radians [0, 5/6τ]. Don't allow a full rotation because that keeps hue == 0 and
-	// hue == 1 from mapping to the same color.
-	var rad = hue * τ * 5/6;
-	rad *= 0.75;  // increase frequency to 2/3 cycle per rad
+  // Map hue [0, 1] to radians [0, 5/6τ]. Don't allow a full rotation because that keeps hue == 0 and
+  // hue == 1 from mapping to the same color.
+  var rad = hue * τ * 5/6;
+  rad *= 0.75;  // increase frequency to 2/3 cycle per rad
 
-	var s = Math.sin(rad);
-	var c = Math.cos(rad);
-	var r = Math.floor(Math.max(0, -c) * 255);
-	var g = Math.floor(Math.max(s, 0) * 255);
-	var b = Math.floor(Math.max(c, 0, -s) * 255);
-	return [r, g, b, a];
+  var s = Math.sin(rad);
+  var c = Math.cos(rad);
+  var r = Math.floor(Math.max(0, -c) * 255);
+  var g = Math.floor(Math.max(s, 0) * 255);
+  var b = Math.floor(Math.max(c, 0, -s) * 255);
+  return [r, g, b, a];
 }
 
 var BOUNDARY = 0.45;
@@ -62,9 +62,9 @@ var fadeToWhite = colorInterpolator(sinebowColor(1.0, 0), [255, 255, 255]);
  * @returns {Array} [r, g, b, a]
  */
 function extendedSinebowColor(i, a) {
-	return i <= BOUNDARY ?
-		sinebowColor(i / BOUNDARY, a) :
-		fadeToWhite((i - BOUNDARY) / (1 - BOUNDARY), a);
+  return i <= BOUNDARY ?
+    sinebowColor(i / BOUNDARY, a) :
+    fadeToWhite((i - BOUNDARY) / (1 - BOUNDARY), a);
 }
 
 exports.extendedSinebowColor = extendedSinebowColor;
--- demo_modules/wind/util.js
+++ demo_modules/wind/util.js
@@ -18,22 +18,23 @@ limitations under the License.
 // https://github.com/cambecc/earth.
 
 /**
- * @returns {Boolean} true if the specified value is not null and not
- *     undefined.
+ * @returns {Boolean} true if the specified value is not null and not undefined.
  */
 function isValue(x) {
   return x !== null && x !== undefined;
 }
 
 /**
- * @returns {Number} returns remainder of floored division, i.e., floor(a / n). Useful for consistent modulo
- *          of negative numbers. See http://en.wikipedia.org/wiki/Modulo_operation.
+ * @returns {Number} returns remainder of floored division, i.e., floor(a / n).
+ *     Useful for consistent modulo of negative numbers.
+ *     See http://en.wikipedia.org/wiki/Modulo_operation.
  */
 function floorMod(a, n) {
-	var f = a - n * Math.floor(a / n);
-	// HACK: when a is extremely close to an n transition, f can be equal to n. This is bad because f must be
-	//       within range [0, n). Check for this corner case. Example: a:=-1e-16, n:=10. What is the proper fix?
-	return f === n ? 0 : f;
+  var f = a - n * Math.floor(a / n);
+  // HACK: when a is extremely close to an n transition, f can be equal to n.
+  // This is bad because f must be within range [0, n). Check for this corner
+  // case. Example: a:=-1e-16, n:=10. What is the proper fix?
+  return f === n ? 0 : f;
 }
 
 
