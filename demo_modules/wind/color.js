6d8334894feb8edb7dd87ee07b5618add2b4b727
Brian Turnbull <brian.m.turnbull@gmail.com>
2017-09-23 15:19:50 -0500

    s/var/const, remove recursive import, rename confusing unicode variable name.

--- demo_modules/wind/color.js
+++ demo_modules/wind/color.js
@@ -17,13 +17,11 @@ limitations under the License.
 // at https://earth.nullschool.net and its open source code:
 // https://github.com/cambecc/earth.
 
-const color = require('demo_modules/wind/color');
-
-const τ = 2 * Math.PI;
+const tau = 2 * Math.PI;
 
 function colorInterpolator(start, end) {
-  var r = start[0], g = start[1], b = start[2];
-  var Δr = end[0] - r, Δg = end[1] - g, Δb = end[2] - b;
+  const r = start[0], g = start[1], b = start[2];
+  const Δr = end[0] - r, Δg = end[1] - g, Δb = end[2] - b;
   return function(i, a) {
     return [Math.floor(r + i * Δr), Math.floor(g + i * Δg), Math.floor(b + i * Δb), a];
   };
@@ -40,19 +38,19 @@ function colorInterpolator(start, end) {
 function sinebowColor(hue, a) {
   // Map hue [0, 1] to radians [0, 5/6τ]. Don't allow a full rotation because that keeps hue == 0 and
   // hue == 1 from mapping to the same color.
-  var rad = hue * τ * 5/6;
+  var rad = hue * tau * 5/6;
   rad *= 0.75;  // increase frequency to 2/3 cycle per rad
 
-  var s = Math.sin(rad);
-  var c = Math.cos(rad);
-  var r = Math.floor(Math.max(0, -c) * 255);
-  var g = Math.floor(Math.max(s, 0) * 255);
-  var b = Math.floor(Math.max(c, 0, -s) * 255);
+  const s = Math.sin(rad);
+  const c = Math.cos(rad);
+  const r = Math.floor(Math.max(0, -c) * 255);
+  const g = Math.floor(Math.max(s, 0) * 255);
+  const b = Math.floor(Math.max(c, 0, -s) * 255);
   return [r, g, b, a];
 }
 
-var BOUNDARY = 0.45;
-var fadeToWhite = colorInterpolator(sinebowColor(1.0, 0), [255, 255, 255]);
+const BOUNDARY = 0.45;
+const fadeToWhite = colorInterpolator(sinebowColor(1.0, 0), [255, 255, 255]);
 
 /**
  * Interpolates a sinebow color where 0 <= i <= j, then fades to white where j < i <= 1.
