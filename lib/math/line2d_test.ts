17be42bba55b66d1e4738a85795b48327d912f9f
Matt Handley <applmak@gmail.com>
2022-10-13 13:39:07 +0200

    Convert tests to use Deno's testing framework.
    - This works because these tests don't require the browser anyway.
    - Also remove some now-unused dependencies.

---
+++ lib/math/line2d_test.ts
@@ -0,0 +1,111 @@
+import chai from "https://cdn.skypack.dev/chai@4.3.4?dts";
+import { describe, it } from "https://deno.land/std@0.159.0/testing/bdd.ts";
+import {
+  distanceToSegment,
+  intersection,
+  intersects,
+  onSegment,
+} from "./line2d.js";
+
+const expect = chai.expect;
+
+describe("onSegment", () => {
+  it("returns true when a point is on a segment", () => {
+    const b = onSegment({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 0.5 });
+    expect(b).to.be.true;
+  });
+  it("returns false when a point is on a segment", () => {
+    const b = onSegment({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0.1, y: 0.5 });
+    expect(b).to.be.false;
+  });
+});
+
+describe("intersection", () => {
+  it("returns null when lines are parallel", () => {
+    const r = intersection({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: -3, y: 0 }, {
+      x: -4,
+      y: -1,
+    });
+    expect(r).to.be.null;
+  });
+  it("returns null when lines are the same", () => {
+    const r = intersection({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: 5, y: 6 }, {
+      x: 6,
+      y: 7,
+    });
+    expect(r).to.be.null;
+  });
+  it("returns null when lines intersect, but segments do not", () => {
+    const r = intersection({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: 6, y: 8 }, {
+      x: 7,
+      y: 7,
+    });
+    expect(r).to.be.null;
+  });
+  it("returns a valid intersection point when lines intersect", () => {
+    const r = intersection({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: 5, y: 7 }, {
+      x: 6,
+      y: 6,
+    });
+    expect(r).to.deep.equal({ p: { x: 5.5, y: 6.5 }, u: 0.5, v: 0.5 });
+  });
+});
+
+describe("intersects", () => {
+  it("returns false when lines are parallel", () => {
+    const r = intersects({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: -3, y: 0 }, {
+      x: -4,
+      y: -1,
+    });
+    expect(r).to.be.false;
+  });
+  it("returns false when lines are the same", () => {
+    const r = intersects({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: 5, y: 6 }, {
+      x: 6,
+      y: 7,
+    });
+    expect(r).to.be.false;
+  });
+  it("returns false when lines intersect, but segments do not", () => {
+    const r = intersects({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: 6, y: 8 }, {
+      x: 7,
+      y: 7,
+    });
+    expect(r).to.be.false;
+  });
+  it("returns true when lines intersect", () => {
+    const r = intersects({ x: 5, y: 6 }, { x: 6, y: 7 }, { x: 5, y: 7 }, {
+      x: 6,
+      y: 6,
+    });
+    expect(r).to.be.true;
+  });
+});
+
+describe("distanceToSegment", () => {
+  it("returns 0 when point is on segment", () => {
+    const d = distanceToSegment({ x: 3, y: 4 }, { x: 7, y: -5 }, {
+      x: 7,
+      y: -5,
+    });
+    expect(d).to.equal(0);
+  });
+  it("returns distance to point when segment is of 0 length", () => {
+    const d = distanceToSegment({ x: 3, y: 4 }, { x: 3, y: 4 }, { x: 0, y: 0 });
+    expect(d).to.equal(5);
+  });
+  it("returns distance to endpoint when out of skew", () => {
+    const d = distanceToSegment({ x: -4, y: -1 }, { x: -4, y: 0 }, {
+      x: -1,
+      y: 4,
+    });
+    expect(d).to.equal(5);
+  });
+  it("returns distance correctly", () => {
+    const d = distanceToSegment({ x: -4, y: -1 }, { x: -4, y: 0 }, {
+      x: -1,
+      y: -0.5,
+    });
+    expect(d).to.equal(3);
+  });
+});
---
+++ lib/math/vector2d_test.ts
@@ -0,0 +1,119 @@
+import chai from "https://cdn.skypack.dev/chai@4.3.4?dts";
+import { describe, it } from "https://deno.land/std@0.159.0/testing/bdd.ts";
+import {
+  add,
+  crossMag,
+  dist,
+  dist2,
+  dot,
+  len,
+  len2,
+  lerp,
+  rot,
+  scale,
+  side,
+  sub,
+} from "./vector2d.js";
+
+const expect = chai.expect;
+
+describe("dot", () => {
+  it("returns the right answer", () => {
+    const r = dot({ x: 5, y: 4 }, { x: 3, y: 2 });
+    expect(r).to.equal(23);
+  });
+});
+
+describe("len", () => {
+  it("returns the right answer", () => {
+    const r = len({ x: 4, y: 3 });
+    expect(r).to.equal(5);
+  });
+});
+
+describe("len2", () => {
+  it("returns the right answer", () => {
+    const r = len2({ x: 1, y: -4 });
+    expect(r).to.equal(17);
+  });
+});
+
+describe("dist", () => {
+  it("returns the right answer", () => {
+    const r = dist({ x: 5, y: -8 }, { x: 2, y: -4 });
+    expect(r).to.equal(5);
+  });
+});
+
+describe("dist2", () => {
+  it("returns the right answer", () => {
+    const r = dist2({ x: 2, y: 1 }, { x: -1, y: 0 });
+    expect(r).to.equal(10);
+  });
+});
+
+describe("sub", () => {
+  it("returns the right answer", () => {
+    const r = sub({ x: 2, y: 1 }, { x: -1, y: 0 });
+    expect(r).to.deep.equal({ x: 3, y: 1 });
+  });
+});
+
+describe("crossMag", () => {
+  it("returns the right answer", () => {
+    const r = crossMag({ x: 2, y: -3 }, { x: 5, y: 1 });
+    expect(r).to.equal(17);
+  });
+});
+
+describe("side", () => {
+  it("returns 0 for on-the-line cases", () => {
+    const s = side({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 0.5 });
+    expect(s).to.equal(0);
+  });
+  it("returns 0 for on-the-point cases", () => {
+    const s = side({ x: 5, y: -7 }, { x: 10, y: 1 }, { x: 10, y: 1 });
+    expect(s).to.equal(0);
+  });
+  it("returns < 1 for to-the-right-of cases", () => {
+    const s = side({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 0.5 });
+    expect(s).to.be.lessThan(0);
+  });
+  it("returns > 1 for to-the-left-of cases", () => {
+    const s = side({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 0.5 });
+    expect(s).to.be.greaterThan(0);
+  });
+});
+
+describe("lerp", () => {
+  it("returns the right answer", () => {
+    const p = lerp({ x: 3, y: -2 }, { x: 5, y: 0 }, 0.5);
+    expect(p).to.deep.equal({ x: 4, y: -1 });
+  });
+});
+
+describe("add", () => {
+  it("returns the right answer", () => {
+    const p = add({ x: 7, y: 4 }, { x: -3, y: 0 });
+    expect(p).to.deep.equal({ x: 4, y: 4 });
+  });
+});
+
+describe("scale", () => {
+  it("returns the right answer", () => {
+    const p = scale({ x: 7, y: 4 }, 2);
+    expect(p).to.deep.equal({ x: 14, y: 8 });
+  });
+});
+
+describe("rot", () => {
+  it("returns the same vec when angle is 0", () => {
+    const p = rot({ x: 1, y: 0 }, 0);
+    expect(p).to.deep.equal({ x: 1, y: 0 });
+  });
+  it("returns a rotated vec when angle is 90°", () => {
+    const p = rot({ x: 1, y: 0 }, Math.PI / 2);
+    expect(p.x).to.be.closeTo(0, 6);
+    expect(p.y).to.be.closeTo(1, 6);
+  });
+});
