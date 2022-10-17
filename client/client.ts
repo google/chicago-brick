ddf4f99c82ca900410042bd698e1fdd2ad2565f9
Matt Handley <applmak@gmail.com>
2022-10-17 15:03:58 +0200

    Convert client.js to .ts
    - add some types
    - be really lazy because omg this has taken forever.

--- client/client.ts
+++ client/client.ts
@@ -13,24 +13,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-import * as monitor from './monitoring/monitor.ts';
-import * as network from './network/network.ts';
-import * as stateManager from '/client/network/state_manager.ts';
-import {isStringWithOptions, makeConsoleLogger} from '/lib/console_logger.ts';
-import {addLogger} from '/lib/log.ts';
-import {now} from './util/time.ts';
-import {errorLogger} from './util/error_logger.ts';
-import {ClientModulePlayer} from '/client/modules/client_module_player.ts';
-import {ClientModule} from './modules/module.ts';
+import * as monitor from "./monitoring/monitor.ts";
+import * as network from "./network/network.ts";
+import * as stateManager from "./network/state_manager.ts";
+import {
+  isStringWithOptions,
+  makeConsoleLogger,
+} from "../lib/console_logger.ts";
+import { addLogger } from "../lib/log.ts";
+import { now } from "./util/time.ts";
+import { errorLogger } from "./util/error_logger.ts";
+import { ClientModulePlayer } from "./modules/client_module_player.ts";
+import { ClientModule } from "./modules/module.ts";
+import { WS } from "../lib/websocket.ts";
 
 addLogger(makeConsoleLogger((...strings) => {
   const processedStrs = [];
   const css = [];
   for (const str of strings) {
-    if (isStringWithOptions(str)) {  
+    if (isStringWithOptions(str)) {
       processedStrs.push(str.str);
       if (str.options.bold) {
-        css.push('font-weight: bolder');
+        css.push("font-weight: bolder");
       }
       if (str.options.backgroundColor) {
         css.push(`background-color: ${str.options.backgroundColor}`);
@@ -39,7 +43,7 @@ addLogger(makeConsoleLogger((...strings) => {
       processedStrs.push(str);
       if (css.length) {
         // Only add a '' css if we already have something in the css box.
-        css.push('');
+        css.push("");
       }
     }
   }
@@ -49,23 +53,26 @@ addLogger(errorLogger);
 
 // Open our socket to the server.
 network.init();
-stateManager.init(network);
+stateManager.init(network as unknown as WS);
 
-if (new URL(window.location.href).searchParams.get('monitor')) {
+if (new URL(window.location.href).searchParams.get("monitor")) {
   monitor.enable();
 }
 
-const modulePlayer = new ClientModulePlayer;
+const modulePlayer = new ClientModulePlayer();
 
 // Server has asked us to load a new module.
-network.on('loadModule',
-    bits => modulePlayer.playModule(ClientModule.deserialize(bits)));
+network.on(
+  "loadModule",
+  (bits) => modulePlayer.playModule(ClientModule.deserialize(bits)),
+);
 
-network.on('takeSnapshot', async req => {
-  if (modulePlayer.oldModule &&
-      modulePlayer.oldModule.instance &&
-      modulePlayer.oldModule.instance.surface) {
-    const image = modulePlayer.oldModule.instance.surface.takeSnapshot();
+network.on("takeSnapshot", async (req) => {
+  const oldModule = modulePlayer.oldModule as ClientModule;
+  if (
+    (oldModule?.instance as any)?.surface
+  ) {
+    const image = (oldModule.instance! as any).surface.takeSnapshot();
     if (image) {
       // You can't draw an imagedata, so we convert to an imagebitmap.
       const WIDTH = 192;
@@ -77,15 +84,15 @@ network.on('takeSnapshot', async req => {
 
       // We can't get the data of a bitmap, so we make a new canvas to get
       // back an imagedata.
-      const canvas = document.createElement('canvas');
+      const canvas = document.createElement("canvas");
       canvas.width = WIDTH;
       canvas.height = HEIGHT;
-      const context = canvas.getContext('2d');
+      const context = canvas.getContext("2d");
       context.drawImage(bitmap, 0, 0);
       const smallData = context.getImageData(0, 0, WIDTH, HEIGHT);
 
       // And now, we get the array itself.
-      network.send('takeSnapshotRes', {
+      network.send("takeSnapshotRes", {
         data: Array.from(smallData.data),
         width: smallData.width,
         ...req,
@@ -93,6 +100,6 @@ network.on('takeSnapshot', async req => {
       return;
     }
   }
-  console.error('snapshot failed', req);
-  network.send('takeSnapshotRes', {...req});
+  console.error("snapshot failed", req);
+  network.send("takeSnapshotRes", { ...req });
 });
