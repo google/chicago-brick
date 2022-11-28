082f8a75e6db1328cf2c3cb907b67864f312333f
Matt Handley <applmak@gmail.com>
2022-11-28 13:53:45 +0100

    Add stars module.

---
+++ demo_modules/stars/interfaces.ts
@@ -0,0 +1,17 @@
+export const V = 0.001;
+export const MAX_NEW_STARS = 100000;
+
+export interface Star {
+  x: number;
+  y: number;
+  z: number;
+  index: number;
+  spawnTime: number;
+}
+
+declare global {
+  interface EmittedEvents {
+    "stars:new-star": (stars: Star[]) => void;
+    "stars:set-warp": (factor: number) => void;
+  }
+}
---
+++ demo_modules/stars/stars.fs
@@ -0,0 +1,26 @@
+uniform float time;
+
+varying float startTimeV;
+varying float z;
+varying float index;
+
+const float TIME = 500.0f;
+const float FADE_OUT_DISTANCE = 0.1;
+
+void main() {
+  // Fade in the point over some time.
+  float fadeInAlpha = clamp((time - startTimeV) / TIME, 0.0, 1.0);
+
+  // Fade out as we get close to camera. The camera plane is at -0.1.
+  float fadeOutAlpha = 1.0 - clamp((z + 1.0) / -FADE_OUT_DISTANCE, 0.0, 1.0);
+
+  vec4 normalColor =
+      mix(vec4(1.0, 0.95, 0.95, 1.0), vec4(0.95, 1.0, 0.95, 1.0), index);
+
+  vec4 fadeInColor = mix(vec4(0.0), normalColor, fadeInAlpha);
+
+  vec4 fadeOutColor = mix(fadeInColor, vec4(0.0), fadeOutAlpha);
+
+  // Set color
+  gl_FragColor = fadeOutColor;
+}
